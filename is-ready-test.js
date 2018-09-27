ebed5cc9b0ab16090e59d3c615f295afd5faa04b
PreziosiR <rpreziosi00@gmail.com>
2018-09-27 15:14:10 +0200

    ajout de are-ready qui teste en parallèle l'existence de nodes_modules et du fichier readme

--- is-ready-test.js
+++ is-ready-test.js
@@ -1,13 +1,15 @@
 const ready = require('./is-ready')
 const chalk = require('chalk');
+const path = `${__dirname}/node_modules`
 
-ready.isReady().then(function(value) {
+//si la promesse est tenue on affiche maybe en jaune
+ready.isReady(path).then(function(value) {
     console.log(chalk.yellow("maybe"))
     console.log(value)
     process.exit(0)
 })
 //sinon on affiche not ready en rouge
-ready.isReady().catch(function(value) {
+ready.isReady(path).catch(function(value) {
     //si erreur on ne peut utiliser chalk, on utilise le code couleur rouge \x1b[31m
     console.log('\x1b[31m', "not ready")
     console.log(value)
--- is-ready.js
+++ is-ready.js
@@ -1,8 +1,6 @@
 const fs = require('fs')
 
-const path = `${__dirname}/node_modules`
-
-module.exports.isReady = function isReady() {
+module.exports.isReady = function isReady(path) {
     return new Promise(function(resolve, reject) {
         fs.stat(path, function(err, stats) {    
             if(err) {
